525fe121e121c2c5160ec398e153e5319fd63c74
hasan814 <h.mousavi910@gmail.com>
2024-10-09 09:54:03 +0330

    Added MongooseConfig

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import "./src/config/mongoose.config.js";
+
+dotenv.config();
 
 async function main() {
   const app = express();
+  const port = process.env.PORT;
   app.listen(3000, () => {
-    console.log("Server:http//localhost:3000");
+    console.log(`Server:http//localhost:${port}`);
   });
 }
 
