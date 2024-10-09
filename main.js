ba4961962b7cb23b9bead818ed2b9810820c1536
hasan814 <h.mousavi910@gmail.com>
2024-10-09 15:37:25 +0330

    Added Error Handler

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+import NotFoundHandler from "./src/common/exception/not-found.handler.js";
 import SwaggerConfig from "./src/config/swagger.config.js";
 import mainRouter from "./src/app.routes.js";
 import express from "express";
 import dotenv from "dotenv";
 
 import "./src/config/mongoose.config.js";
+import AllExceptionHandler from "./src/common/exception/all-exception.handler.js";
 
 dotenv.config();
 
@@ -11,9 +13,11 @@ async function main() {
   const app = express();
   const port = process.env.PORT;
   app.use(express.json());
-  app.use(express.urlencoded());
-  SwaggerConfig(app);
+  app.use(express.urlencoded({ extended: true }));
   app.use(mainRouter);
+  SwaggerConfig(app);
+  NotFoundHandler(app);
+  AllExceptionHandler(app);
   app.listen(process.env.PORT, () => {
     console.log(`Server: http://localhost:${port}`);
   });
---
+++ src/common/exception/not-found.handler.js
@@ -0,0 +1,7 @@
+const NotFoundHandler = (app) => {
+  app.use((req, res, next) => {
+    res.status(404).json({ message: "Not Found Route" });
+  });
+};
+
+export default NotFoundHandler;
